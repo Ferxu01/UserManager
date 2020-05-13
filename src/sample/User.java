f8ff7e1b2dd2b47fecc92605922f6564075e7aec
Fernando Ortega <ferortjim@gmail.com>
2020-05-13 20:15:39 +0200

    Update User.java

--- src/sample/User.java
+++ src/sample/User.java
@@ -4,7 +4,7 @@ public class User {
     protected String username;
     protected String password;
 
-    public User(String username, String password) {
+    public abstract User(String username, String password) {
         this.username = username;
         this.password = password;
     }
